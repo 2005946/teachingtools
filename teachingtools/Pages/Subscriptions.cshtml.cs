81609c897079bf695a81a6bf130b429f4b2718d7
MICHAL NIZIO <2005946@chester.ac.uk>
2023-04-13 22:15:31 +0100

    Moved the subscriptions page behind authorisation

--- teachingtools/Pages/Subscriptions.cshtml.cs
+++ teachingtools/Pages/Subscriptions.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -5,6 +6,7 @@ using teachingtools.Data;
 
 namespace teachingtools.Pages
 {
+    [Authorize]
     public class SubscriptionsModel : PageModel
     {
         private AppDbContext _db;
